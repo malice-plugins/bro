91df783d427765bef48e44961b18b1c20e657ddf
blacktop <blacktop@users.noreply.github.com>
2017-02-19 16:04:09 -0500

    clear out scan.go

--- scan.go
+++ scan.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/fatih/structs"
-	"github.com/gorilla/mux"
-	"github.com/maliceio/go-plugin-utils/clitable"
-	"github.com/maliceio/go-plugin-utils/database/elasticsearch"
 	"github.com/maliceio/go-plugin-utils/utils"
-	"github.com/parnurzeal/gorequest"
 	"github.com/urfave/cli"
 )
 
@@ -30,189 +19,9 @@ var BuildTime string
 var path string
 
 const (
-	name     = "clamav"
-	category = "av"
+	name     = "bro"
+	category = "net"
 )
-
-type pluginResults struct {
-	ID   string      `json:"id" structs:"id,omitempty"`
-	Data ResultsData `json:"clamav" structs:"clamav"`
-}
-
-// ClamAV json object
-type ClamAV struct {
-	Results ResultsData `json:"clamav"`
-}
-
-// ResultsData json object
-type ResultsData struct {
-	Infected bool   `json:"infected" structs:"infected"`
-	Result   string `json:"result" structs:"result"`
-	Engine   string `json:"engine" structs:"engine"`
-	Known    string `json:"known" structs:"known"`
-	Updated  string `json:"updated" structs:"updated"`
-	Error    string `json:"error" structs:"error"`
-	Markdown string `json:"markdown" structs:"markdown"`
-}
-
-func assert(err error) {
-	if err != nil {
-		log.WithFields(log.Fields{
-			"plugin":   name,
-			"category": category,
-			"path":     path,
-		}).Fatal(err)
-	}
-}
-
-// AvScan performs antivirus scan
-func AvScan(timeout int) ClamAV {
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
-	defer cancel()
-
-	return ClamAV{
-		Results: ParseClamAvOutput(utils.RunCommand(ctx, "/usr/bin/clamscan", "--stdout", path)),
-	}
-}
-
-// ParseClamAvOutput convert clamav output into ClamAV struct
-func ParseClamAvOutput(clamout string, err error) ResultsData {
-
-	if err != nil {
-		return ResultsData{Error: err.Error()}
-	}
-
-	log.WithFields(log.Fields{
-		"plugin":   name,
-		"category": category,
-		"path":     path,
-	}).Debug("ClamAV Output: ", clamout)
-
-	clamAV := ResultsData{}
-
-	lines := strings.Split(clamout, "\n")
-	// Extract AV Scan Result
-	result := lines[0]
-	if len(result) != 0 {
-		pathAndResult := strings.Split(result, ":")
-		if strings.Contains(pathAndResult[1], "OK") {
-			clamAV.Infected = false
-		} else {
-			clamAV.Infected = true
-			clamAV.Result = strings.TrimSpace(strings.TrimRight(pathAndResult[1], "FOUND"))
-		}
-	} else {
-		fmt.Println("[ERROR] empty scan result: ", result)
-		os.Exit(2)
-	}
-	// Extract Clam Details from SCAN SUMMARY
-	for _, line := range lines[1:] {
-		if len(line) != 0 {
-			keyvalue := strings.Split(line, ":")
-			if len(keyvalue) != 0 {
-				switch {
-				case strings.Contains(keyvalue[0], "Known viruses"):
-					clamAV.Known = strings.TrimSpace(keyvalue[1])
-				case strings.Contains(line, "Engine version"):
-					clamAV.Engine = strings.TrimSpace(keyvalue[1])
-				}
-			}
-		}
-	}
-
-	clamAV.Updated = getUpdatedDate()
-
-	return clamAV
-}
-
-func updateAV(ctx context.Context) error {
-	fmt.Println("Updating ClamAV...")
-	fmt.Println(utils.RunCommand(ctx, "freshclam"))
-	// Update UPDATED file
-	t := time.Now().Format("20060102")
-	err := ioutil.WriteFile("/opt/malice/UPDATED", []byte(t), 0644)
-	return err
-}
-
-func getUpdatedDate() string {
-	if _, err := os.Stat("/opt/malice/UPDATED"); os.IsNotExist(err) {
-		return BuildTime
-	}
-	updated, err := ioutil.ReadFile("/opt/malice/UPDATED")
-	assert(err)
-	return string(updated)
-}
-
-func printMarkDownTable(clamav ClamAV, toString bool) string {
-	table := clitable.New([]string{"Infected", "Result", "Engine", "Updated"})
-	table.AddRow(map[string]interface{}{
-		"Infected": clamav.Results.Infected,
-		"Result":   clamav.Results.Result,
-		"Engine":   clamav.Results.Engine,
-		// "Known":    clamav.Results.Known,
-		"Updated": clamav.Results.Updated,
-	})
-	table.Markdown = true
-
-	if toString {
-		return table.String("ClamAV")
-	}
-	fmt.Println("#### ClamAV")
-	table.Print()
-	return ""
-}
-
-func printStatus(resp gorequest.Response, body string, errs []error) {
-	fmt.Println(body)
-}
-
-func webService() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/scan", webAvScan).Methods("POST")
-	log.Info("web service listening on port :3993")
-	log.Fatal(http.ListenAndServe(":3993", router))
-}
-
-func webAvScan(w http.ResponseWriter, r *http.Request) {
-
-	r.ParseMultipartForm(32 << 20)
-	file, header, err := r.FormFile("malware")
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, "Please supply a valid file to scan.")
-		log.Error(err)
-	}
-	defer file.Close()
-
-	log.Debug("Uploaded fileName: ", header.Filename)
-
-	tmpfile, err := ioutil.TempFile("/malware", "web_")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name()) // clean up
-
-	data, err := ioutil.ReadAll(file)
-
-	if _, err = tmpfile.Write(data); err != nil {
-		log.Fatal(err)
-	}
-	if err = tmpfile.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Do AV scan
-	path = tmpfile.Name()
-	clamav := AvScan(60)
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-
-	if err := json.NewEncoder(w).Encode(clamav); err != nil {
-		log.Fatal(err)
-	}
-}
 
 func main() {
 
@@ -221,12 +30,12 @@ func main() {
 	cli.AppHelpTemplate = utils.AppHelpTemplate
 	app := cli.NewApp()
 
-	app.Name = "clamav"
+	app.Name = "bro"
 	app.Author = "blacktop"
 	app.Email = "https://github.com/blacktop"
 	app.Version = Version + ", BuildTime: " + BuildTime
 	app.Compiled, _ = time.Parse("20060102", BuildTime)
-	app.Usage = "Malice ClamAV Plugin"
+	app.Usage = "Malice Bro Plugin"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "verbose, V",
@@ -260,79 +69,14 @@ func main() {
 			EnvVar: "MALICE_TIMEOUT",
 		},
 	}
-	app.Commands = []cli.Command{
-		{
-			Name:    "update",
-			Aliases: []string{"u"},
-			Usage:   "Update virus definitions",
-			Action: func(c *cli.Context) error {
-				ctx, cancel := context.WithTimeout(
-					context.Background(),
-					time.Duration(c.Int("timeout"))*time.Second,
-				)
-				defer cancel()
-
-				return updateAV(ctx)
-			},
-		},
-		{
-			Name:  "web",
-			Usage: "Create a ClamAV scan web service",
-			Action: func(c *cli.Context) error {
-				webService()
-				return nil
-			},
-		},
-	}
 	app.Action = func(c *cli.Context) error {
-
-		var err error
 
 		if c.Bool("verbose") {
 			log.SetLevel(log.DebugLevel)
 		}
 
 		if c.Args().Present() {
-			path, err = filepath.Abs(c.Args().First())
-			assert(err)
-
-			if _, err = os.Stat(path); os.IsNotExist(err) {
-				assert(err)
-			}
-
-			clamav := AvScan(c.Int("timeout"))
-
-			// upsert into Database
-			elasticsearch.InitElasticSearch(elastic)
-			elasticsearch.WritePluginResultsToDatabase(elasticsearch.PluginResults{
-				ID:       utils.Getopt("MALICE_SCANID", utils.GetSHA256(path)),
-				Name:     name,
-				Category: category,
-				Data:     structs.Map(clamav.Results),
-			})
-
-			if c.Bool("table") {
-				printMarkDownTable(clamav, false)
-			} else {
-				// add markdown output as a string
-				clamav.Results.Markdown = printMarkDownTable(clamav, true)
-				// convert to JSON
-				clamavJSON, err := json.Marshal(clamav)
-				assert(err)
-				if c.Bool("post") {
-					request := gorequest.New()
-					if c.Bool("proxy") {
-						request = gorequest.New().Proxy(os.Getenv("MALICE_PROXY"))
-					}
-					request.Post(os.Getenv("MALICE_ENDPOINT")).
-						Set("X-Malice-ID", utils.Getopt("MALICE_SCANID", utils.GetSHA256(path))).
-						Send(string(clamavJSON)).
-						End(printStatus)
-
-					return nil
-				}
-				fmt.Println(string(clamavJSON))
-			}
+			log.Info("NOT DONE YET (WIP)")
 		} else {
 			log.Fatal(fmt.Errorf("Please supply a file to scan with malice/clamav"))
 		}
@@ -340,5 +84,5 @@ func main() {
 	}
 
 	err := app.Run(os.Args)
-	assert(err)
+	utils.Assert(err)
 }
